a211c4e290a0365ffa8d675f0076db22f2c53476
ipek <cw_ipk@outlook.com>
2020-05-19 14:01:00 +0300

    Alarm Receiver Added to Download Layout

---
+++ app/src/main/java/com/ipk/mobilodev/AlarmReceiver.java
@@ -0,0 +1,26 @@
+package com.ipk.mobilodev;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.widget.Toast;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Alarm Çalıyor!", Toast.LENGTH_LONG).show();
+
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null)
+        {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+
+    }
+}
--- app/src/main/java/com/ipk/mobilodev/DownloadActivity.java
+++ app/src/main/java/com/ipk/mobilodev/DownloadActivity.java
@@ -2,6 +2,11 @@ package com.ipk.mobilodev;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -13,12 +18,20 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.Random;
 
+//alarm da burada
 public class DownloadActivity extends AppCompatActivity {
+    //alarm
+    private Switch startAlarmBtn;
+    private TimePickerDialog timePickerDialog;
+    final static int REQUEST_CODE = 1;
 
     Button btn;
     private ProgressBar progressBar;
@@ -50,9 +63,78 @@ public class DownloadActivity extends AppCompatActivity {
                 new MyTask().execute(100);
             }
         });
+
+        //alarm
+        startAlarmBtn = findViewById(R.id.startAlarmBtn);
+        startAlarmBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(startAlarmBtn.isChecked()){
+                    openPickerDialog(true);
+                }else{
+                    cancelAlarm();
+                    Toast.makeText(getApplicationContext(), "Alarm İptal Edildi", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+    //alarm
+    private void openPickerDialog(boolean is24hour) {
+
+        Calendar calendar = Calendar.getInstance();
+
+        timePickerDialog = new TimePickerDialog(
+                DownloadActivity.this,
+                onTimeSetListener,
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE),
+                is24hour);
+        timePickerDialog.setTitle("Alarm Ayarla");
+
+        timePickerDialog.show();
+    }
+    //alarm
+    TimePickerDialog.OnTimeSetListener onTimeSetListener
+            = new TimePickerDialog.OnTimeSetListener(){
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+            Calendar calNow = Calendar.getInstance();
+            Calendar calSet = (Calendar) calNow.clone();
+
+            calSet.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            calSet.set(Calendar.MINUTE, minute);
+            calSet.set(Calendar.SECOND, 0);
+            calSet.set(Calendar.MILLISECOND, 0);
+
+            if(calSet.compareTo(calNow) <= 0){
+
+                calSet.add(Calendar.DATE, 1);
+            }
+
+            setAlarm(calSet);
+        }};
+    //alarm
+    private void setAlarm(Calendar alarmCalender){
+
+
+        Toast.makeText(getApplicationContext(),"Alarm Ayarlandı!",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getBaseContext(), AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getBaseContext(), REQUEST_CODE, intent, 0);
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, alarmCalender.getTimeInMillis(), pendingIntent);
+
+    }
+    private void cancelAlarm(){
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), REQUEST_CODE, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.cancel(pendingIntent);
     }
 
-
+    //download
     class MyTask extends AsyncTask<Integer, Integer, String> {
 
         @Override
